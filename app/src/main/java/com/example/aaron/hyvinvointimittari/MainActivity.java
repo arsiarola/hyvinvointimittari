f8eb7585a3eece3bceaff18fb61e3e3f8eff9034
aaronha <aaronha@metropolia.fi>
2018-12-05 09:52:50 +0200

    test

--- app/src/main/java/com/example/aaron/hyvinvointimittari/MainActivity.java
+++ app/src/main/java/com/example/aaron/hyvinvointimittari/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.aaron.hyvinvointimittari;
 
 import android.app.Activity;
@@ -7,13 +8,17 @@ import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
-
+import java.util.Calendar;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.TimeZone;
+
 /*ideas for project:
   maybe a little minigame if henVointi is low, which could be harder based on how low it is. e.g. tap some stuff on the screen
   , jump over obstacles or something like in the google dinosaurs minigame. could then increase henVoinit
@@ -27,8 +32,12 @@ import java.util.ArrayList;
  */
 public class MainActivity extends AppCompatActivity {
     //Declaring variables
-    private int fysVointi = 0;
+    public static int fysVointi = 0;
     public static float henVointi = 50;
+    private int weeklyFysVointi = 0;
+    public static float weeklyHenVointi = 50;
+    private int alltimeFysVointi = 0;
+    public static float alltimeHenVointi = 50;
     private Button suoritusButton;
     private Button userSettings;
     private AutoCompleteTextView oloTilaText;
@@ -44,15 +53,21 @@ public class MainActivity extends AppCompatActivity {
     private MainActivity thisActivity;
     private Button mittariButton;
     private static final String PREF = "DATA";
-    @RequiresApi(api = Build.VERSION_CODES.O)
+    private Calendar kalenteri;
+    private int previousDate;
+    private int day;
+    private static String alltimeFys = "alltimeFysVointi";
+    private static String alltimeHen = "alltimeHenVointi";
+    private static String weeklyHen = "weeklyHenVointi";
+    private static String weeklyFys = "weeklyFysVointi";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //initializing variables
-        SharedPreferences prefGet = getSharedPreferences(PREF, Activity.MODE_PRIVATE);
-        henVointi = prefGet.getFloat("henVointi",50);
-        fysVointi = prefGet.getInt("fysVointi", 0);
+
+
+
         mittariButton =  findViewById(R.id.mittari);
         userSettings = findViewById(R.id.userSettings);
         thisActivity = this;
@@ -61,31 +76,54 @@ public class MainActivity extends AppCompatActivity {
         suoritus = findViewById(R.id.fysSuoritus);
         suoritusMaara =findViewById(R.id.fysSuoritusMaara);
         suoritusButton = findViewById(R.id.fysSuoritusButton);
+        kalenteri = Calendar.getInstance(TimeZone.getTimeZone("EET"));
+        kalenteri.setFirstDayOfWeek(Calendar.MONDAY);
+        day = kalenteri.get(kalenteri.DAY_OF_WEEK);
+
+        SharedPreferences prefGet = getSharedPreferences(PREF, Activity.MODE_PRIVATE);
+        henVointi = prefGet.getFloat("henVointi",50f);
+        fysVointi = prefGet.getInt("fysVointi", 0);
+        weeklyHenVointi = prefGet.getFloat("weeklyHenVointi", 50f);
+        weeklyFysVointi = prefGet.getInt("weeklyFysVointi", 0);
+        alltimeFysVointi = prefGet.getInt("alltimeFysVointi", 0);
+        alltimeHenVointi = prefGet.getFloat("alltimeHenVointi", 50f);
+        previousDate = prefGet.getInt("previousDate", day);
+        Log.d("äksdee",previousDate + " ja " + day);
+        if(previousDate != day){
+            henVointi = 50;
+            fysVointi = 0;
+            if(day == 2){
+                weeklyFysVointi = 0;
+                weeklyHenVointi = 50;
+            }
+
+        }
+
         //luodaan eri olotiloja
-        olot= new ArrayList<olotilat>();
-        olot.add(new olotilat("surullinen",0.6)); //could add an array of strings instead so same word differently spelled etc works
-        olot.add(new olotilat("iloinen",1.6));
-        olot.add(new olotilat("masentunut",0.3));
-        olot.add(new olotilat("itsetuhoinen",0.1));
-        olot.add(new olotilat("hyvä",1.3));
-        olot.add(new olotilat("innostunut",1.8));
-        olot.add(new olotilat("vihainen",0.7));
-        olot.add(new olotilat("ärsyyntynyt",0.85));
-        olot.add(new olotilat("pelko",0.25));
-        olot.add(new olotilat("rakastunut",2.0));
-        olot.add(new olotilat("pettynyt",0.7));
+        olot = new ArrayList<olotilat>();
+        olot.add(new olotilat("surullinen", 0.6)); //could add an array of strings instead so same word differently spelled etc works
+        olot.add(new olotilat("iloinen", 1.6));
+        olot.add(new olotilat("masentunut", 0.3));
+        olot.add(new olotilat("itsetuhoinen", 0.1));
+        olot.add(new olotilat("hyvä", 1.3));
+        olot.add(new olotilat("innostunut", 1.8));
+        olot.add(new olotilat("vihainen", 0.7));
+        olot.add(new olotilat("ärsyyntynyt", 0.85));
+        olot.add(new olotilat("pelko", 0.25));
+        olot.add(new olotilat("rakastunut", 2.0));
+        olot.add(new olotilat("pettynyt", 0.7));
         olot.add(new olotilat("turhautunut", 0.8));
         olot.add(new olotilat("ahdistunut", 0.8));
         olot.add(new olotilat("kateellinen", 0.7));
-        olot.add(new olotilat("häpeä",0.6));
-        olot.add(new olotilat("mukava",1.5));
-        olot.add(new olotilat("epämukava",2/3));
-        olot.add(new olotilat("rauhallinen",1.4));
-        olot.add(new olotilat("ylpeä",1.7));
+        olot.add(new olotilat("häpeä", 0.6));
+        olot.add(new olotilat("mukava", 1.5));
+        olot.add(new olotilat("epämukava", 2 / 3));
+        olot.add(new olotilat("rauhallinen", 1.4));
+        olot.add(new olotilat("ylpeä", 1.7));
 
         //luodaan eri suorituksia
         suoritukset = new ArrayList<suoritukset>();
-        suoritukset.add(new suoritukset("juoksu",50));
+        suoritukset.add(new suoritukset("juoksu", 50));
         suoritukset.add(new suoritukset("tennis", 30));
         suoritukset.add(new suoritukset("sulkapallo", 25));
         suoritukset.add(new suoritukset("jalkapallo", 30));
@@ -94,40 +132,40 @@ public class MainActivity extends AppCompatActivity {
         suoritukset.add(new suoritukset("tanssi", 20));
         suoritukset.add(new suoritukset("jääkiekko", 40));
         suoritukset.add(new suoritukset("sprintti", 55));
-        suoritukset.add(new suoritukset("kävely",15));
-        suoritukset.add(new suoritukset("keihäänheitto",25));
-        suoritukset.add(new suoritukset("hiihto",45));
-        suoritukset.add(new suoritukset("koripallo",40));
-        suoritukset.add(new suoritukset("sähly",40));
-        suoritukset.add(new suoritukset("lentopallo",40));
-        suoritukset.add(new suoritukset("rullaluistelu",35));
-        suoritukset.add(new suoritukset("pyöräily, kevyt",30));
-        suoritukset.add(new suoritukset("pyöräily, raskas",50));
-        suoritukset.add(new suoritukset("kamppailu",45));
-        suoritukset.add(new suoritukset("laskettelu",40));
-        suoritukset.add(new suoritukset("moottoriurheilu",15));
-        suoritukset.add(new suoritukset("pesäpallo",40));
-        suoritukset.add(new suoritukset("venyttely",10));
+        suoritukset.add(new suoritukset("kävely", 15));
+        suoritukset.add(new suoritukset("keihäänheitto", 25));
+        suoritukset.add(new suoritukset("hiihto", 45));
+        suoritukset.add(new suoritukset("koripallo", 40));
+        suoritukset.add(new suoritukset("sähly", 40));
+        suoritukset.add(new suoritukset("lentopallo", 40));
+        suoritukset.add(new suoritukset("rullaluistelu", 35));
+        suoritukset.add(new suoritukset("pyöräily, kevyt", 30));
+        suoritukset.add(new suoritukset("pyöräily, raskas", 50));
+        suoritukset.add(new suoritukset("kamppailu", 45));
+        suoritukset.add(new suoritukset("laskettelu", 40));
+        suoritukset.add(new suoritukset("moottoriurheilu", 15));
+        suoritukset.add(new suoritukset("pesäpallo", 40));
+        suoritukset.add(new suoritukset("venyttely", 10));
 
         //luodaan olotila- ja suoritusvaihtoehdoista nimilistat
         ArrayList<String> suoritusNimet = new ArrayList<String>();
-        for(int i=0; i < suoritukset.size(); i++){
+        for (int i = 0; i < suoritukset.size(); i++) {
             suoritusNimet.add(suoritukset.get(i).getOlotila());
         }
         ArrayList<String> olotilaNimet = new ArrayList();
-        for(int i = 0; i < olot.size(); i++){
+        for (int i = 0; i < olot.size(); i++) {
             olotilaNimet.add(olot.get(i).getOlotila());
         }
 
         //luodaan lista kirjoitettavista vaihtoehdoista
         ArrayAdapter<String> adapter = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1,suoritusNimet);
+                (this, android.R.layout.simple_list_item_1, suoritusNimet);
         suoritus.setThreshold(1); //will start working from first character
         suoritus.setAdapter(adapter);
 
         //luodaan lista kirjoitettavista vaihtoehdoista
         ArrayAdapter<String> adapter2 = new ArrayAdapter<String>
-                (this, android.R.layout.simple_list_item_1,olotilaNimet);
+                (this, android.R.layout.simple_list_item_1, olotilaNimet);
         oloTilaText.setThreshold(1); //will start working from first character
         oloTilaText.setAdapter(adapter2);
 
@@ -135,50 +173,59 @@ public class MainActivity extends AppCompatActivity {
         suoritusButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                wrongText=true; //muuttuja ilmaisee onko teksti väärä
-                textFail=false; //muuttuja ilmaisee onko kentässä numero
-                double time=0;
+                wrongText = true; //muuttuja ilmaisee onko teksti väärä
+                textFail = false; //muuttuja ilmaisee onko kentässä numero
+                double time = 0;
                 try { //yrittää muuttaa syötteen numeroksi
                     time = Double.parseDouble(suoritusMaara.getText().toString());
                 } catch (NumberFormatException e) {
-                    textFail=true;
+                    textFail = true;
                 }
-                if(textFail==false){
-                    for (int i =0; i< suoritukset.size(); i++){
-                        if(suoritus.getText().toString().equals(suoritukset.get(i).getOlotila())){
-                            fysVointi+= time * suoritukset.get(i).getMultiplier();
+                if (textFail == false) {
+                    for (int i = 0; i < suoritukset.size(); i++) {
+                        if (suoritus.getText().toString().equals(suoritukset.get(i).getOlotila())) {
+                            fysVointi += time * suoritukset.get(i).getMultiplier();
+                            weeklyFysVointi += time * suoritukset.get(i).getMultiplier();
+                            alltimeFysVointi += time * suoritukset.get(i).getMultiplier();
                             suoritus.setText("");
-                            wrongText=false;
+                            wrongText = false;
                             break;
                         }
                     }
                 }
 
-                if(wrongText==true){
+                if (wrongText == true) {
                     suoritus.setText("virhe");
                 }
             }
         });
 
 
-
         //lisää uuden olotilan
         oloTilaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                wrongText=true;
-                for (int i =0; i< olot.size(); i++){
-                    if(oloTilaText.getText().toString().equals(olot.get(i).getOlotila())){
-                        henVointi*=olot.get(i).getMultiplier();
-                        if(Math.round(henVointi) == 0) {
+                wrongText = true;
+                for (int i = 0; i < olot.size(); i++) {
+                    if (oloTilaText.getText().toString().equals(olot.get(i).getOlotila())) {
+                        henVointi *= olot.get(i).getMultiplier();
+                        if (Math.round(henVointi) == 0) {
                             henVointi = 1;
                         }
+                        weeklyHenVointi *= olot.get(i).getMultiplier();
+                        if (Math.round(weeklyHenVointi) == 0) {
+                            weeklyHenVointi = 1;
+                        }
+                        alltimeHenVointi *= olot.get(i).getMultiplier();
+                        if (Math.round(alltimeHenVointi) == 0) {
+                            alltimeHenVointi = 1;
+                        }
                         oloTilaText.setText("");
-                        wrongText=false;
+                        wrongText = false;
                         break;
                     }
                 }
-                if(wrongText==true){
+                if (wrongText == true) {
                     oloTilaText.setText("virhe");
                 }
             }
@@ -191,8 +238,12 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(thisActivity, MittariActivity.class);
                 // EditText editText = (EditText) findViewById(R.id.editText);
                 // String message = editText.getText().toString();
-                intent.putExtra("extra",henVointi);
+                intent.putExtra("extra", henVointi);
                 intent.putExtra("fysVointi", fysVointi);
+                intent.putExtra(weeklyFys, weeklyFysVointi);
+                intent.putExtra(weeklyHen, weeklyHenVointi);
+                intent.putExtra(alltimeHen,alltimeHenVointi);
+                intent.putExtra(alltimeFys,alltimeFysVointi);
                 startActivity(intent);
             }
         });
@@ -210,11 +261,18 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-    public void onPause(){
-        SharedPreferences prefPut = getSharedPreferences(PREF,Activity.MODE_PRIVATE);
+
+    public void onPause() {
+        //tallentaa tietoja
+        SharedPreferences prefPut = getSharedPreferences(PREF, Activity.MODE_PRIVATE);
         SharedPreferences.Editor prefEditor = prefPut.edit();
         prefEditor.putFloat("henVointi", henVointi);
         prefEditor.putInt("fysVointi", fysVointi);
+        prefEditor.putInt(weeklyFys, weeklyFysVointi);
+        prefEditor.putFloat(weeklyHen, weeklyHenVointi);
+        prefEditor.putInt(alltimeFys, alltimeFysVointi);
+        prefEditor.putFloat(alltimeHen, alltimeHenVointi);
+        prefEditor.putInt("previousDate", kalenteri.get(kalenteri.DAY_OF_WEEK));
         prefEditor.commit();
         super.onPause();
     }
--- app/src/main/java/com/example/aaron/hyvinvointimittari/MittariActivity.java
+++ app/src/main/java/com/example/aaron/hyvinvointimittari/MittariActivity.java
@@ -9,50 +9,80 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class MittariActivity extends AppCompatActivity {
-
+    private Button paiva;
+    private Button viikko;
+    private Button alusta;
+    private float weeklyHenVointi;
+    private int weeklyFysVointi;
+    private float alltimeHenVointi;
+    private int alltimeFysVointi;
+    private float henVointi;
+    private int fysVointi;
+    private ProgressBar hen;
+    private ProgressBar fys;
+    private TextView fyysinen;
+    private TextView henkinen;
+    private float uusiHenk;
+    private float viikkoUusiHenk;
+    private float alustaUusiHenk;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mittari);
-        ProgressBar fys = findViewById(R.id.progressBarFys);
-        ProgressBar hen = findViewById(R.id.progressBarHenk);
+        fys = findViewById(R.id.progressBarFys);
+        hen = findViewById(R.id.progressBarHenk);
         Intent intent = getIntent();
-        float henVointi = intent.getFloatExtra("extra", 0);
-        int fysVointi = intent.getIntExtra("fysVointi", 0);
-        TextView fyysinen = findViewById(R.id.fysLuku);
+        henVointi = intent.getFloatExtra("extra", 50);
+        fysVointi = intent.getIntExtra("fysVointi", 0);
+        weeklyHenVointi = intent.getFloatExtra("weeklyHenVointi",50);
+        weeklyFysVointi = intent.getIntExtra("weeklyFysVointi",0);
+        alltimeHenVointi = intent.getFloatExtra("alltimeHenVointi",50);
+        alltimeFysVointi = intent.getIntExtra("alltimeFysVointi",50);
+        fyysinen = findViewById(R.id.fysLuku);
         fyysinen.setText(Integer.toString(fysVointi));
-        TextView henkinen = findViewById(R.id.henkLuku);
-        float uusiHenk = henVointi - 50;
+        henkinen = findViewById(R.id.henkLuku);
+        uusiHenk = henVointi - 50;
         henkinen.setText(Integer.toString(Math.round(uusiHenk)));
+        viikkoUusiHenk = weeklyHenVointi -50;
+        alustaUusiHenk = alltimeHenVointi -50;
         hen.setProgress(Math.round(henVointi));
         fys.setProgress(fysVointi);
 
         //buttonit
 
-        Button paiva = findViewById(R.id.päivä);
+        paiva = findViewById(R.id.päivä);
         paiva.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                hen.setProgress(Math.round(henVointi));
+                fys.setProgress(fysVointi);
+                fyysinen.setText(Integer.toString(fysVointi));
+                henkinen.setText(Integer.toString(Math.round(uusiHenk)));
             }
         });
 
-        Button viikko = findViewById(R.id.viikko);
+        viikko = findViewById(R.id.viikko);
         viikko.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                hen.setProgress(Math.round(weeklyHenVointi));
+                fys.setProgress(weeklyFysVointi);
+                fyysinen.setText(Integer.toString(weeklyFysVointi));
+                henkinen.setText(Integer.toString(Math.round(viikkoUusiHenk)));
             }
         });
 
-        Button allTime = findViewById(R.id.alustalähtien);
-        allTime.setOnClickListener(new View.OnClickListener() {
+        alusta = findViewById(R.id.alustalähtien);
+        alusta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                hen.setProgress(Math.round(alltimeHenVointi));
+                fys.setProgress(alltimeFysVointi);
+                fyysinen.setText(Integer.toString(alltimeFysVointi));
+                henkinen.setText(Integer.toString(Math.round(alustaUusiHenk)));
 
             }
         });
 
     }
 }
-
